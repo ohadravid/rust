77e14880fea7e59b1a41959d5093236425fc394f
Ohad Ravid <ohad.rv@gmail.com>
2026-01-20 11:46:16 -0800

    Update the windows tls test

--- src/tools/miri/tests/pass/tls/windows-tls.rs
+++ src/tools/miri/tests/pass/tls/windows-tls.rs
@@ -36,6 +36,32 @@ fn main() {
         println!("dtor2");
     }
 
+    extern "system" fn dtor3(val: *mut c_void) {
+        assert_eq!(val.addr(), 1);
+        println!("dtor3");
+    }
+
+    // Check dtor behaviors during thread exit.
+    extern "system" fn dtor_behaviors(val: *mut c_void) {
+        println!("dtor_behaviors");
+        
+        // Updating a different fls slot's value doesn't cause their dtor to run, if it already did.
+        let fls_key_1 = val as _;
+        assert_eq!(unsafe { FlsSetValue(fls_key_1, ptr::without_provenance_mut(1)) }, TRUE);
+
+        // Registering new fls slots doesn't cause their dtor to run.
+        let a_new_key_in_dtor = unsafe { FlsAlloc(Some(dtor1)) };
+        assert_eq!(unsafe { FlsSetValue(a_new_key_in_dtor, ptr::without_provenance_mut(1)) }, TRUE);
+    }
+
+    extern "system" fn dtor_behaviors_2(val: *mut c_void) {
+        println!("dtor_behaviors_2");
+        
+        // Updating a different fls slot's value affect their dtor's run, if it is hasn't yet.
+        let fls_key_4 = val as _;
+        assert_eq!(unsafe { FlsSetValue(fls_key_4, ptr::without_provenance_mut(1)) }, TRUE);
+    }
+    
     assert_eq!(unsafe { IsThreadAFiber() }, FALSE);
 
     thread::spawn(|| {
@@ -47,6 +73,14 @@ fn main() {
         let fls_key_2 = unsafe { FlsAlloc(Some(dtor2)) };
         assert_eq!(unsafe { FlsSetValue(fls_key_2, ptr::without_provenance_mut(1)) }, TRUE);
         assert_eq!(unsafe { FlsGetValue(fls_key_2).addr() }, 1);
+        
+        let fls_key_3 = unsafe { FlsAlloc(Some(dtor_behaviors)) };
+        assert_eq!(unsafe { FlsSetValue(fls_key_3, ptr::without_provenance_mut(fls_key_1 as _)) }, TRUE);
+
+        let fls_key_4 = unsafe { FlsAlloc(Some(dtor_behaviors_2)) };
+        let fls_key_5 = unsafe { FlsAlloc(Some(dtor3)) };
+        assert_eq!(unsafe { FlsSetValue(fls_key_4, ptr::without_provenance_mut(fls_key_5 as _)) }, TRUE);
+
         println!("exiting thread");
     })
     .join()
